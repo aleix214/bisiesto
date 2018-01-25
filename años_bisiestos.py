388f5149a3619ccb2d0284d8e896e70f43e347ec
aleix214 <35691131+aleix214@users.noreply.github.com>
2018-01-25 13:41:56 +0100

    Update años_bisiestos.py

--- años_bisiestos.py
+++ años_bisiestos.py
@@ -1,8 +1,13 @@
 # coding: utf­8
 
-num=int(input("Dime un año:   "))
-if(num % 4 == 0 and num % 100 != 0) or num % 400 == 0):
- print("Si es bisiesto")
-else:
- print("No es bisiesto")
+def bisiesto(num):
+	if(num % 4 == 0 and num % 100 != 0) or num % 400 == 0:
+		print("Si es bisiesto")
+	else:
+		print("No es bisiesto")
+
+bisiesto(2004)
+bisiesto(2300)
+bisiesto(1995)
+
 
